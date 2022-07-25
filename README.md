18f46f34eb2e32124e17d88f69b09f0c62231b8f
HunterR <gonidzze@gmail.com>
2022-07-25 21:12:49 +0300

    Add asciinem to Readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/HunterGan/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/HunterGan/frontend-project-lvl2/actions)
+
+# Example for 2 JSON files
+
+[![asciicast](https://asciinema.org/a/aSeJGrj42Ec9NWnPwknUmNtYX.svg)](https://asciinema.org/a/aSeJGrj42Ec9NWnPwknUmNtYX)
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import fs from 'fs';
+import genDiff from '../src/index.js';
+import path from 'path';
+import process from 'process';
+//import { readFileSync } from 'node:fs';
 const program = new Command();
+
 
 program
     .description('Compares two configuration files and shows a difference.')
@@ -8,4 +14,9 @@ program
     .option('-f, --format <type>', 'output format')
     .argument('<filepath1>')
     .argument('<filepath2>')
+    .action((filepath1, filepath2) => {
+        const fileData1 = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), filepath1)));
+        const fileData2 = JSON.parse(fs.readFileSync(filepath2));
+        console.log(genDiff(fileData1, fileData2));
+    });
 program.parse();
