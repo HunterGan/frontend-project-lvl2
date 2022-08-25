770bf5a5b1e8e7728ebc6d3674b8d45a4c96d27a
HunterR <gonidzze@gmail.com>
2022-08-25 15:12:51 +0300

    buildDiff refactoring

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -1,33 +1,25 @@
 import _ from 'lodash';
 
-const buildDiff = (object1, object2) => {
-  const keys1 = Object.keys(object1) ?? [];
-  const keys2 = Object.keys(object2) ?? [];
-  const commonKeys = _.sortedUniq(_.sortBy([...keys1, ...keys2]));
+const buildDiff = (object1, object2 = object1) => {
+  if (!_.isObject(object1)) return object1;
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  const commonKeys = _.sortBy(_.uniq([...keys1, ...keys2]));
   const diffs = commonKeys.map((key) => {
-    if (!_.has(object1, key) || !_.has(object2, key)) {
-      const argBuilder = () => {
-        const operationType = (!_.has(object1, key)) ? 'added' : 'removed';
-        const nextValue = (operationType === 'added') ? object2[key] : object1[key];
-        const value = (_.isObject(nextValue)) ? buildDiff(nextValue, nextValue) : nextValue;
-        return { type: operationType, key, value };
-      };
-      return argBuilder();
+    if (!_.has(object1, key)) {
+      return { type: 'added', key, value: buildDiff(object2[key]) };
+    }
+    if (!_.has(object2, key)) {
+      return { type: 'removed', key, value: buildDiff(object1[key]) };
     }
     if (_.isObject(object1[key]) && _.isObject(object2[key])) {
       return { type: 'unchanged', key, value: buildDiff(object1[key], object2[key]) };
     }
     if (object1[key] !== object2[key]) {
-      const valueBuilder = () => {
-        if (_.isObject(object1[key])) return [buildDiff(object1[key], object1[key]), object2[key]];
-        if (_.isObject(object2[key])) return [object1[key], buildDiff(object2[key], object2[key])];
-        return [object1[key], object2[key]];
-      };
-      return { type: 'updated', key, value: valueBuilder() };
+      return { type: 'updated', key, value: [buildDiff(object1[key]), buildDiff(object2[key])] };
     }
     return { type: 'unchanged', key, value: object1[key] };
   });
   return diffs;
 };
-
 export default buildDiff;
--- src/utils.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-import process from 'process';
-
-export const fileExt = (pathToFile) => path.extname(pathToFile).toLowerCase();
-export const fullPath = (pathTofile) => path.resolve(process.cwd(), pathTofile);
-export const fileRead = (pathToFile) => fs.readFileSync(pathToFile, 'utf-8');
