1e4fe9f7457806b87f706a5dd1b721502cf59298
HunterR <gonidzze@gmail.com>
2022-09-06 18:43:41 +0300

    printPlain edit codeclimat

--- src/formatters/printPlain.js
+++ src/formatters/printPlain.js
@@ -7,32 +7,29 @@ const toStringValue = (value) => {
 };
 
 export default (diffs) => {
-  const buildPrint = (current, fullPath = []) => {
-    const diffLines = current.map((currentChild) => {
-      const keyType = currentChild.type;
-      const currentPath = [...fullPath, currentChild.key].join('.');
-      switch (keyType) {
-        case 'nested': {
-          return buildPrint(currentChild.children, [currentPath]);
-        }
-        case 'updated': {
-          return `Property '${currentPath}' was updated. From ${toStringValue(currentChild.value[0])} to ${toStringValue(currentChild.value[1])}`;
-        }
-        case 'added': {
-          return `Property '${currentPath}' was added with value: ${toStringValue(currentChild.value)}`;
-        }
-        case 'removed': {
-          return `Property '${currentPath}' was removed`;
-        }
-        case 'unchanged': {
-          return null;
-        }
-        default: {
-          throw new Error('Something went wrong');
-        }
+  const buildPrint = (current, fullPath = []) => current.map((currentChild) => {
+    const keyType = currentChild.type;
+    const currentPath = [...fullPath, currentChild.key].join('.');
+    switch (keyType) {
+      case 'nested': {
+        return buildPrint(currentChild.children, [currentPath]);
       }
-    });
-    return _.flattenDeep(diffLines.filter((line) => line)).join('\n');
-  };
+      case 'updated': {
+        return `Property '${currentPath}' was updated. From ${toStringValue(currentChild.value[0])} to ${toStringValue(currentChild.value[1])}`;
+      }
+      case 'added': {
+        return `Property '${currentPath}' was added with value: ${toStringValue(currentChild.value)}`;
+      }
+      case 'removed': {
+        return `Property '${currentPath}' was removed`;
+      }
+      case 'unchanged': {
+        return null;
+      }
+      default: {
+        throw new Error('Something went wrong');
+      }
+    }
+  }).filter((line) => line).join('\n');
   return buildPrint(diffs);
 };
